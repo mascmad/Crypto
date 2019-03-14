9d2981e7a60f32915ce64515331bbccd2948823b
Mason Maddox <mmaddox19@outlook.com>
2019-03-14 18:36:26 -0500

    Initial commit

---
+++ rot_brute.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+import sys
+# brute forces a rot-x cipher through all 26 letters of the alphabet
+
+message = ''
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+msg = sys.argv
+msg.pop(0)
+message = str(msg)
+
+# array for positions of capital characters in the message
+caps = []
+
+# loop through the entire message to find what characters are caps
+for l in range(0, len(message)):
+    if message[l].isupper():
+        caps.append(l)
+
+#print(caps)
+
+message = message.lower()
+
+# begin looping through all the alphabet (x is the key)
+for x in range(1, 26):
+    str_out = ''
+    print(str(x) + ':\t', end='')
+
+    # begin looping through the message
+    for i in range(0, len(message)):
+        # error-checking
+        try:
+            # index of the shifted letter
+            f = alphabet.index(message[i]) + x
+            if 0 > f:       # if the number would go under the number of chars in the array
+                a = f + 26
+                str_out += alphabet[a]
+            elif 26 <= f:   # if the number would go above the number of chars in the array
+                b = f - 26
+                str_out += alphabet[b]
+            else:
+                str_out += alphabet[f]
+        except ValueError:  # if the character is a number or otherwise non-alphanumeric
+            str_out += message[i]
+    for n in caps:
+        str_out = str_out.replace(str_out[n], str_out[n].capitalize())
+    str_out = str_out.replace("[", "").replace("'", "").replace(",", "").replace("]", "")
+    print(str_out)
